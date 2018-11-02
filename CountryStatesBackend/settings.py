0e3c283e25eba834724a6c1c0d43fd90d8146d5d
Emily  Murphy <Emily.Murphy@emurphy.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-11-02 20:50:57 +0000

    WIP: Make Models

--- CountryStatesBackend/settings.py
+++ CountryStatesBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'country.apps.CountryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- country/models.py
+++ country/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    country_name = models.CharField(max_length=200)
+    country_code = models.CharField(max_length=200)
+
+class State(models.Model):
+    state_name = models.CharField(max_length=200)
+    state_code = models.CharField(max_length=200)
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
 
