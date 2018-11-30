6ffdba3cbe7346440e5e1b3e568025dbfcadb2a3
Emily  Murphy <Emily.Murphy@emurphy.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-11-30 18:50:00 +0000

    Alphabetize API

--- country/models.py
+++ country/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100, blank=True, default='')
-    code = models.CharField(max_length=2, blank=True, default='')
+    name = models.CharField(max_length=100, blank=False, default='')
+    code = models.CharField(max_length=2, blank=False, default='')
 
 
 
 class State(models.Model):
-    name = models.CharField(max_length=100, blank=True, default='')
-    code = models.CharField(max_length=2, blank=True, default='')
+    name = models.CharField(max_length=100, blank=False, default='')
+    code = models.CharField(max_length=2, blank=False, default='')
     countryId = models.ForeignKey(Country, on_delete=models.CASCADE,related_name='states')
 
